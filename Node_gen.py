e1cced0d7fa7f55e1362283c15f483038ca2312e
lhfaguiar <lais.harumi.f@gmail.com>
2017-10-27 19:59:07 -0200

    Create Node_gen.py

---
+++ Node_gen.py
@@ -0,0 +1,19 @@
+"""
+Esse arquivo gera o no de camada escondida
+
+
+"""
+
+# Imports
+import pandas as pd
+import math
+
+
+def Hidden_node_gen():
+    """
+    Gera um no de camada escondida da rede neural
+    """
+    node = ''
+
+
+    return node
